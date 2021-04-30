2e2067cae3aca1e5d86385d39be497f1efa9db9f
Ben Black <weepingwillowben@gmail.com>
2021-04-30 10:33:25 -0400

    implemented max entropy nash solver

--- nash_solver.py
+++ nash_solver.py
@@ -1,19 +1,29 @@
 import cvxpy as cp
 import numpy as np
 
-def equilibrium(A):
+def max_entropy_nash(A):
     x = cp.Variable(A.shape[0])
     z = cp.Variable()
 
     bounds = [
         z - A.T @ x <= 0,
         cp.sum(x) == 1,
-        x >= 0]
+        x >= 0
+    ]
     cp.Problem(cp.Maximize(z), bounds).solve()
+    minimax_value = z.value
+
+    x = cp.Variable(A.shape[0])
+    bounds = [
+        A.T @ x == minimax_value,
+        cp.sum(x) == 1,
+        x >= 0
+    ]
+    cp.Problem(cp.Maximize(cp.sum(cp.entr(x))), bounds).solve()
 
     # x = pd.Series(x.value, A.index)
     # y = pd.Series(bounds[0].dual_value, A.columns)
-    return x.value, bounds[0].dual_value
+    return x.value#, bounds[0].dual_value
 
 matrix = np.array([
     [0,1,-1],
